273ee06eed2e51e40f4e8c5265978e2a74083a90
Andreas Gantner <andreasgantner@yahoo.de>
2020-06-03 17:13:55 +0200

    [Enhancement] Loading monolog package
    + Loading and implementing the monolog composer package.
    - Renaming prepareDB.php to index.php

---
+++ API/api/index.php
@@ -0,0 +1,14 @@
+<!--
+-- index.php
+--
+-- Script checking the structure of the current api version. If
+-- the structure is missing this script will provide the functionality
+-- to create the api structure and DB. If it exists and is valid for
+-- the current version it'll show a message.
+--
+-- author: Andreas G.
+-- last edit / by: 2020-06-03 / Andreas G.
+-->
+<?php
+
+?>
