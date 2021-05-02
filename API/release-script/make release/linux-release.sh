d7d0338fd275be866efaa84a4f76fcef8b6ba490
Kontr0x <55395652+kontr0x@users.noreply.github.com>
2021-05-02 16:33:13 +0200

    Changed linux virtual env to be downloaded and not being deliverd
    - removed python virtual env for linux

--- API/release-script/make release/linux-release.sh
+++ API/release-script/make release/linux-release.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-../pyversion/python394_linux/python ../pyscript/release.py
+if [ -d "../pyversion/python394" ]; then
+	../pyversion/python394/python ../pyscript/release.py	
+else
+	wget wget https://www.python.org/ftp/python/3.9.4/Python-3.9.4.tar.xz
+	tar -xf Python-3.9.4.tar.xz
+	rm ./Python-3.9.4.tar.xz
+	cp -r ./Python-3.9.4 ../pyversion/python394
+	rm -r ./Python-3.9.4
+	cd ../pyversion/python394/
+	./configure; make
+	../pyversion/python394/python ../pyscript/release.py
+fi
