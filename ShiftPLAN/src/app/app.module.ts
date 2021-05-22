6adbc7e74757f7165ade019b39f98518526cbbee
TrickyTreat <58302057+TrickyTreat@users.noreply.github.com>
2021-05-22 12:49:53 +0200

    Initial commit for Frontend
    + creating the angular project
    
    - deleting the WebApp folder

---
+++ ShiftPLAN/src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+export function translateHttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateHttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  availableLng = ['en', 'de'];
+
+  //start the translation service
+  constructor(private translateService: TranslateService) {
+      //defines the default language
+      let tmpLng: string = 'en';
+  
+      //gets the default browser language
+      const currentLng = window.navigator.language.substring(0,2);
+  
+      if (this.availableLng.includes(currentLng)) {
+        tmpLng = currentLng;
+      }
+  
+      translateService.setDefaultLang(tmpLng);
+  }
+ }
