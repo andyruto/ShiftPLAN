c9d8be1302bec5d91f6bd5660c183ed94653dbc3
Anne <an055@hdm-stuttgart.de>
2021-05-26 18:01:21 +0200

    Adding wizard-permission component

---
+++ ShiftPLAN/src/app/modules/start/wizard-permission/wizard-permission.component.ts
@@ -0,0 +1,23 @@
+/**
+ * wizard-permission.component.ts
+ * 
+ * Main typescript class for the wizard-permission component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-05-26 / Anne Naumann
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wizard-permission',
+  templateUrl: './wizard-permission.component.html',
+  styleUrls: ['./wizard-permission.component.scss']
+})
+export class WizardPermissionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
