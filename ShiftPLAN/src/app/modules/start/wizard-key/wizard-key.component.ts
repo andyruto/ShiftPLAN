dffd8eedf012a9305c526ea60cb52247ddfdab8c
Anne <an055@hdm-stuttgart.de>
2021-05-26 18:00:45 +0200

    Adding wizard-key component

---
+++ ShiftPLAN/src/app/modules/start/wizard-key/wizard-key.component.ts
@@ -0,0 +1,23 @@
+/**
+ * wizard-key.component.ts
+ * 
+ * Main typescript class for the wizard-key component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-05-26 / Anne Naumann
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wizard-key',
+  templateUrl: './wizard-key.component.html',
+  styleUrls: ['./wizard-key.component.scss']
+})
+export class WizardKeyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
