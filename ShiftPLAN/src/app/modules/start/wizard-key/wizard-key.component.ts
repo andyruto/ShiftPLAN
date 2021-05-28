eb96aa39174c8846dbc9047631b9c5d08c493457
Anne <an055@hdm-stuttgart.de>
2021-05-28 17:12:18 +0200

    Implementing layout of wizard-key component

--- ShiftPLAN/src/app/modules/start/wizard-key/wizard-key.component.ts
+++ ShiftPLAN/src/app/modules/start/wizard-key/wizard-key.component.ts
@@ -4,7 +4,7 @@
  * Main typescript class for the wizard-key component.
  * 
  * author: Anne Naumann
- * last edit / by: 2021-05-26 / Anne Naumann
+ * last edit / by: 2021-05-28 / Anne Naumann
  */
 import { Component, OnInit } from '@angular/core';
 
@@ -16,8 +16,20 @@ import { Component, OnInit } from '@angular/core';
 export class WizardKeyComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
   }
 
+  checkInput(userInput: string): void{
+    let warning = document.getElementById('warning')
+    var inputValid: Boolean = false
+
+    //check Input
+    console.log(userInput)
+
+    if(inputValid){
+      //navigate to next component
+    }else{
+      warning!.style.visibility = 'visible'
+    }
+  }
 }
