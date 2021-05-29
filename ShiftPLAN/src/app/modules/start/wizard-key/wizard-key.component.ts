3bb97fd16e96ed715b572d1fff8356f8f3d549f8
Anne <an055@hdm-stuttgart.de>
2021-05-29 16:10:58 +0200

    Adding translation for wizard-key component

--- ShiftPLAN/src/app/modules/start/wizard-key/wizard-key.component.ts
+++ ShiftPLAN/src/app/modules/start/wizard-key/wizard-key.component.ts
@@ -4,9 +4,10 @@
  * Main typescript class for the wizard-key component.
  * 
  * author: Anne Naumann
- * last edit / by: 2021-05-28 / Anne Naumann
+ * last edit / by: 2021-05-29 / Anne Naumann
  */
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-wizard-key',
@@ -15,8 +16,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WizardKeyComponent implements OnInit {
 
-  constructor() { }
+  title = ''
+  label = ''
+  warning = ''
+  nextButton = ''
+  
+  constructor(private translate : TranslateService) { }
   ngOnInit(): void {
+    this.translate.getTranslation(this.translate.defaultLang).subscribe((translation: any) => { 
+      this.title = translation.WizardKey.Title;
+      this.label = translation.WizardKey.Label;
+      this.warning = translation.WizardKey.Warning;
+      this.nextButton = translation.NextButton;
+    });
   }
 
   checkInput(userInput: string): void{
