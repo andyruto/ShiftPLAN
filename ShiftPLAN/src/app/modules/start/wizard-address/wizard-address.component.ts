4b9c831d7fb873d9b3e180017f74634c83a34fa4
Anne <an055@hdm-stuttgart.de>
2021-05-29 16:00:53 +0200

    Adding translation for wizard-address component

--- ShiftPLAN/src/app/modules/start/wizard-address/wizard-address.component.ts
+++ ShiftPLAN/src/app/modules/start/wizard-address/wizard-address.component.ts
@@ -4,9 +4,10 @@
  * Main typescript class for the wizard-address component.
  * 
  * author: Anne Naumann
- * last edit / by: 2021-05-28 / Anne Naumann
+ * last edit / by: 2021-05-29 / Anne Naumann
  */
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-wizard-address',
@@ -14,9 +15,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wizard-address.component.scss']
 })
 export class WizardAddressComponent implements OnInit {
+
+  title = ''
+  label = ''
+  warning = ''
+  nextButton = ''
   
-  constructor() { }
-  ngOnInit(): void { }
+  constructor(private translate : TranslateService) { }
+  ngOnInit(): void {
+    this.translate.getTranslation(this.translate.defaultLang).subscribe((translation: any) => { 
+      this.title = translation.WizardAddress.Title;
+      this.label = translation.WizardAddress.Label;
+      this.warning = translation.WizardAddress.Warning;
+      this.nextButton = translation.NextButton;
+    });
+   }
   
   checkInput(userInput: string): void{
     let warning = document.getElementById('warning')
