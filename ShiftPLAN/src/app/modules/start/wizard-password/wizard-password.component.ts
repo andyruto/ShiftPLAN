c6a4d20b9a55a739155f39717520394284462976
Anne <an055@hdm-stuttgart.de>
2021-05-29 15:06:11 +0200

    Implementing layout of wizard-password component

--- ShiftPLAN/src/app/modules/start/wizard-password/wizard-password.component.ts
+++ ShiftPLAN/src/app/modules/start/wizard-password/wizard-password.component.ts
@@ -4,20 +4,44 @@
  * Main typescript class for the wizard-password component.
  * 
  * author: Anne Naumann
- * last edit / by: 2021-05-26 / Anne Naumann
+ * last edit / by: 2021-05-29 / Anne Naumann
  */
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-wizard-password',
   templateUrl: './wizard-password.component.html',
   styleUrls: ['./wizard-password.component.scss']
 })
-export class WizardPasswordComponent implements OnInit {
+export class WizardPasswordComponent{
 
-  constructor() { }
+  checkPassword(password: string): void{
+    //TODO: get initial password
+    let initPassword: string = ''
+    let checkDiv = document.getElementById('checkOldPassword')
+    let setDiv = document.getElementById('setNewPassword')
+    let warning = document.getElementById('warningOld')
 
-  ngOnInit(): void {
+    if(initPassword == password){
+      checkDiv!.style.display = 'none'
+      setDiv!.style.display = 'block'
+    }else {
+      warning!.style.visibility = 'visible'
+    }
   }
 
+  setPassword(password: string, passwordCheck: string): void{
+    let warning = document.getElementById('warningNew')
+    var inputValid: Boolean = false
+
+    //check if password secure enough?
+    console.log(password)
+    console.log(passwordCheck)
+
+    if(password == passwordCheck){
+      //navigate to next component
+    }else{
+      warning!.style.visibility = 'visible'
+    }
+  }
 }
