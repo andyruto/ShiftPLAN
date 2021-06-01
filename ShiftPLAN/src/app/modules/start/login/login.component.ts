6823f698db5b06f3231e4dbb19504171572108c8
Anne <an055@hdm-stuttgart.de>
2021-06-01 12:31:28 +0200

    Implementing layout of login component

--- ShiftPLAN/src/app/modules/start/login/login.component.ts
+++ ShiftPLAN/src/app/modules/start/login/login.component.ts
@@ -4,9 +4,10 @@
  * Main typescript class for the login component.
  * 
  * author: Anne Naumann
- * last edit / by: 2021-05-26 / Anne Naumann
+ * last edit / by: 2021-06-01 / Anne Naumann
  */
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
@@ -15,9 +16,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  eyeIcon: string = 'eye-slash'
+  inputType: string = 'password'
+  userName = ''
+  password = ''
+  resetBtn = ''
+  warning = ''
 
+  constructor(private translate : TranslateService) { }
   ngOnInit(): void {
+    this.translate.getTranslation(this.translate.defaultLang).subscribe((translation: any) => { 
+      this.userName = translation.Login.UserName;
+      this.password = translation.Login.Password;
+      this.resetBtn = translation.Login.ResetButton;
+      this.warning = translation.Login.Warning;
+    });
   }
 
+  showPassword(): void{
+    var toggleBtn = document.getElementById('toggleBtn')
+
+    if(toggleBtn?.classList.contains('mat-button-toggle-checked')){
+      this.eyeIcon = 'eye'
+      this.inputType = 'text'
+    }else {
+      this.eyeIcon = 'eye-slash'
+      this.inputType = 'password'
+    }
+  }
+
+  checkLoginValues(inputName: string, inputPassword: string): void{
+    var warning = document.getElementById('warning')
+    //TODO: get values
+    let userName = ''
+    let userPassword = ''
+
+    if(userName == inputName && userPassword == inputPassword){
+      //TODO: navigate to App
+    }else {
+      warning!.style.visibility = 'visible'
+    }
+  }
+
+  resetPassword(): void{
+    //TODO: implement function to reset password
+  }
 }
