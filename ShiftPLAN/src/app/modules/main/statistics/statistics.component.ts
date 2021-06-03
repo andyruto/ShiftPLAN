f5a6385b80c9fd3bf1a7b87d3d7e9a7362f7ba5b
Anne <an055@hdm-stuttgart.de>
2021-06-03 17:36:44 +0200

    Adding statistics component

---
+++ ShiftPLAN/src/app/modules/main/statistics/statistics.component.ts
@@ -0,0 +1,23 @@
+/**
+ * statistics.component.ts
+ * 
+ *  Main typescript class for the statistics component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-06-03 / Anne Naumann
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics',
+  templateUrl: './statistics.component.html',
+  styleUrls: ['./statistics.component.scss']
+})
+export class StatisticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
