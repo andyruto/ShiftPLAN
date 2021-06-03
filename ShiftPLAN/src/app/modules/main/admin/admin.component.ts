aaa7232cd04becf67c6d3c40c013065be6bdf276
Anne <an055@hdm-stuttgart.de>
2021-06-03 17:35:38 +0200

    Adding admin component

---
+++ ShiftPLAN/src/app/modules/main/admin/admin.component.ts
@@ -0,0 +1,23 @@
+/**
+ * admin.component.ts
+ * 
+ *  Main typescript class for the admin component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-06-03 / Anne Naumann
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
