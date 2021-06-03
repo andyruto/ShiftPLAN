142a3e585d44cbd14bbf0c811767055bda2c4795
Anne <an055@hdm-stuttgart.de>
2021-06-03 17:36:31 +0200

    Adding shifts component

---
+++ ShiftPLAN/src/app/modules/main/shifts/shifts.component.ts
@@ -0,0 +1,23 @@
+/**
+ * shifts.component.ts
+ * 
+ *  Main typescript class for the shifts component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-06-03 / Anne Naumann
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shifts',
+  templateUrl: './shifts.component.html',
+  styleUrls: ['./shifts.component.scss']
+})
+export class ShiftsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
