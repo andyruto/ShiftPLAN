05791efb0e64a6c8083c76e7934fdc4507ea543d
Anne <an055@hdm-stuttgart.de>
2021-06-03 17:36:19 +0200

    Adding settings component

---
+++ ShiftPLAN/src/app/modules/main/settings/settings.component.ts
@@ -0,0 +1,23 @@
+/**
+ * settings.component.ts
+ * 
+ *  Main typescript class for the settings component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-06-03 / Anne Naumann
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.scss']
+})
+export class SettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
