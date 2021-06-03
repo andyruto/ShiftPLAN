bcef95a87283aff5030165f6c74e3bb453b1a687
Anne <an055@hdm-stuttgart.de>
2021-06-03 17:37:56 +0200

    Adding routes for new components

--- ShiftPLAN/src/app/app-routing.module.ts
+++ ShiftPLAN/src/app/app-routing.module.ts
@@ -5,13 +5,28 @@
  * for our application.
  * 
  * author: Sascha W.
- * last edit / by: 2021-05-22 / Sascha W.
+ * last edit / by: 2021-06-03 / Anne Naumann
  */
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './modules/main/admin/admin.component';
+import { HomeComponent } from './modules/main/home/home.component';
+import { ProfileComponent } from './modules/main/profile/profile.component';
+import { SettingsComponent } from './modules/main/settings/settings.component';
+import { ShiftsComponent } from './modules/main/shifts/shifts.component';
+import { StatisticsComponent } from './modules/main/statistics/statistics.component';
+import { TasksComponent } from './modules/main/tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'shifts', component: ShiftsComponent},
+  {path: 'tasks', component: TasksComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: 'admin', component: AdminComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
