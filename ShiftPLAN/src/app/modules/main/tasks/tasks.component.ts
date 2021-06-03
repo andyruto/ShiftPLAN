f66f70f233f390db839d68929dd2cbf707668390
Anne <an055@hdm-stuttgart.de>
2021-06-03 17:36:54 +0200

    Adding tasks component

---
+++ ShiftPLAN/src/app/modules/main/tasks/tasks.component.ts
@@ -0,0 +1,23 @@
+/**
+ * tasks.component.ts
+ * 
+ *  Main typescript class for the tasks component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-06-03 / Anne Naumann
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.scss']
+})
+export class TasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
