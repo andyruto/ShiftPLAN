fb27bd1d043049b05ca2b64e5addb115f0825dec
Anne <an055@hdm-stuttgart.de>
2021-06-03 17:35:52 +0200

    Adding home component

---
+++ ShiftPLAN/src/app/modules/main/home/home.component.spec.ts
@@ -0,0 +1,33 @@
+/**
+ * home.component.spec
+ * 
+ * Typescript class containing the tests for the 
+ * home component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-06-03 / Anne Naumann
+ */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HomeComponent } from './home.component';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HomeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ShiftPLAN/src/app/modules/main/home/home.component.ts
@@ -0,0 +1,23 @@
+/**
+ * home.component.ts
+ * 
+ *  Main typescript class for the home component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-06-03 / Anne Naumann
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
