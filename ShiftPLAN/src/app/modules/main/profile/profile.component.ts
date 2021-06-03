c3a83368172255f124165b878d1ad3b17ef62509
Anne <an055@hdm-stuttgart.de>
2021-06-03 17:36:06 +0200

    Adding profile component

---
+++ ShiftPLAN/src/app/modules/main/profile/profile.component.ts
@@ -0,0 +1,23 @@
+/**
+ * profile.component.ts
+ * 
+ *  Main typescript class for the profile component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-06-03 / Anne Naumann
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
