e78f6b3f2058b75fb8fbfb7d4ed1f170ee8dc794
Anne <an055@hdm-stuttgart.de>
2021-06-07 16:12:15 +0200

    Adding settings-info component

---
+++ ShiftPLAN/src/app/modules/main/settings-info/settings-info.component.ts
@@ -0,0 +1,23 @@
+/**
+ * settings-info.component.ts
+ * 
+ *  Main typescript class for the settings-info component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-06-07 / Anne Naumann
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-info',
+  templateUrl: './settings-info.component.html',
+  styleUrls: ['./settings-info.component.scss']
+})
+export class SettingsInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
