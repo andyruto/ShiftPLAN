e879741760fb342b295616926ead69915961ba80
Anne <an055@hdm-stuttgart.de>
2021-06-07 15:56:07 +0200

    Implementing of settings component

--- ShiftPLAN/src/app/modules/main/settings/settings.component.ts
+++ ShiftPLAN/src/app/modules/main/settings/settings.component.ts
@@ -4,7 +4,7 @@
  *  Main typescript class for the settings component.
  * 
  * author: Anne Naumann
- * last edit / by: 2021-06-03 / Anne Naumann
+ * last edit / by: 2021-06-07 / Anne Naumann
  */
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
@@ -23,5 +23,21 @@ export class SettingsComponent implements OnInit {
     this.translate.getTranslation(this.translate.defaultLang).subscribe((translation: any) => { 
       this.title = translation.Toolbar.Title.Settings;
     });
+  }
+
+  navigateToTheme(): void {
+    console.log('click on theme')
+  }
+
+  navigateToAdaptHome(): void {
+    console.log('click on adapt')
+  }
+
+  navigateToPassword(): void {
+    console.log('click on password')
+  }
+
+  navigateToInfo(): void {
+    console.log('click on info')
   }
 }
