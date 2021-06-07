8d752158fb705616c3278eded875d7503119af17
Anne <an055@hdm-stuttgart.de>
2021-06-07 16:15:35 +0200

    Adding settings-password component

---
+++ ShiftPLAN/src/app/modules/main/settings-password/settings-password.component.ts
@@ -0,0 +1,23 @@
+/**
+ * settings-password.component.ts
+ * 
+ *  Main typescript class for the settings-password component.
+ * 
+ * author: Anne Naumann
+ * last edit / by: 2021-06-07 / Anne Naumann
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-password',
+  templateUrl: './settings-password.component.html',
+  styleUrls: ['./settings-password.component.scss']
+})
+export class SettingsPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
