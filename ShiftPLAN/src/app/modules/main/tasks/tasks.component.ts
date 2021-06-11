09bb20fad3d108ee5a33628468360d9b7a09d546
Anne <an055@hdm-stuttgart.de>
2021-06-11 18:15:34 +0200

    Implementing layout and adding translation of tasks component

--- ShiftPLAN/src/app/modules/main/tasks/tasks.component.ts
+++ ShiftPLAN/src/app/modules/main/tasks/tasks.component.ts
@@ -4,24 +4,49 @@
  *  Main typescript class for the tasks component.
  * 
  * author: Anne Naumann
- * last edit / by: 2021-06-03 / Anne Naumann
+ * last edit / by: 2021-06-11 / Anne Naumann
  */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
 
   title = ''
+  searchBarText = ''
 
-  constructor(private translate: TranslateService) { }
+  //test content that needs to be replaced
+  tasks: Array<string> = ['a','b','c','d','e','f','g','h','i','b','c','d','e','f','g','h','i']
+
+  constructor(private translate: TranslateService, private router : Router) { }
+
   ngOnInit(): void {
     this.translate.getTranslation(this.translate.defaultLang).subscribe((translation: any) => { 
       this.title = translation.Toolbar.Title.Tasks;
+      this.searchBarText = translation.Tasks.SearchBarText;
     });
+  }
+
+  ngAfterViewInit(): void{
+    let toolbar =  document.getElementsByTagName('mat-toolbar')[0]
+    let bottomBar = document.getElementsByTagName('app-bottom-bar')[0]
+    let searchBar = document.getElementsByTagName('mat-form-field')[0]
+    var scrollView = document.getElementById('tasksScrollView')
+
+    let screenHeight = window.innerHeight-toolbar.clientHeight-bottomBar.clientHeight-searchBar.clientHeight
+    scrollView!.style.height = screenHeight + 'px'
+  }
+
+  navigateToTask(): void{
+
+  }
+
+  navigateToAddTask(): void{
+
   }
 }
