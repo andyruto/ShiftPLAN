8031c388ed3fa5dc5fc10d83c248667f96aad776
Anne <an055@hdm-stuttgart.de>
2021-06-24 00:43:39 +0200

    Moving files into new file order

--- ShiftPLAN/src/app/modules/main/main-routing.module.ts
+++ ShiftPLAN/src/app/modules/main/main-routing.module.ts
@@ -10,22 +10,23 @@
 
  import { NgModule } from '@angular/core';
  import { RouterModule, Routes } from '@angular/router';
- import { AdminComponent } from './admin/admin.component';
- import { AdminAddComponent } from './admin-add/admin-add.component';
- import { AdminUserComponent } from './admin-user/admin-user.component';
- import { HomeComponent } from './home/home.component';
- import { ProfileComponent } from './profile/profile.component';
- import { SettingsComponent } from './settings/settings.component';
- import { SettingsInfoComponent } from './settings-info/settings-info.component'
- import { SettingsPasswordComponent } from './settings-password/settings-password.component'
- import { SettingsThemeComponent } from './settings-theme/settings-theme.component'
- import { SettingsAdaptScreenComponent } from './settings-adapt-screen/settings-adapt-screen.component'
- import { ShiftsComponent } from './shifts/shifts.component';
- import { StatisticsComponent } from './statistics/statistics.component';
- import { TasksComponent } from './tasks/tasks.component';
- import { TasksAddComponent } from './tasks-add/tasks-add.component';
+ import { AdminComponent } from './components/admin/admin.component';
+ import { AdminAddComponent } from './components/admin-add/admin-add.component';
+ import { AdminUserComponent } from './components/admin-user/admin-user.component';
+ import { HomeComponent } from './components/home/home.component';
+ import { ProfileComponent } from './components/profile/profile.component';
+ import { SettingsComponent } from './components/settings/settings.component';
+ import { SettingsInfoComponent } from './components/settings-info/settings-info.component'
+ import { SettingsPasswordComponent } from './components/settings-password/settings-password.component'
+ import { SettingsThemeComponent } from './components/settings-theme/settings-theme.component'
+ import { SettingsAdaptScreenComponent } from './components/settings-adapt-screen/settings-adapt-screen.component'
+ import { ShiftsComponent } from './components/shifts/shifts.component';
+ import { StatisticsComponent } from './components/statistics/statistics.component';
+ import { TasksComponent } from './components/tasks/tasks.component';
+ import { TasksAddComponent } from './components/tasks-add/tasks-add.component';
+ import { TasksTaskComponent } from './components/tasks-task/tasks-task.component';
  import { MainComponent } from './main.component'
- import { MainNavigationComponent } from './main-navigation/main-navigation.component'
+ import { MainNavigationComponent } from './components/main-navigation/main-navigation.component'
  
  const routes: Routes = [
   { path: '', component: MainComponent, children: [
@@ -44,7 +45,8 @@
     {path: 'settings-adapt-screen', component: SettingsAdaptScreenComponent},
     {path: 'admin-add', component: AdminAddComponent },
     {path: 'admin-user', component: AdminUserComponent },
-    {path: 'tasks-add', component: TasksAddComponent }
+    {path: 'tasks-add', component: TasksAddComponent },
+    {path: 'tasks-task', component: TasksTaskComponent }
   ]}
  ];
  
--- ShiftPLAN/src/app/modules/main/main.module.ts
+++ ShiftPLAN/src/app/modules/main/main.module.ts
@@ -4,7 +4,7 @@
  * Module that contains all main components.
  * 
  * author: Anne Naumann
- * last edit / by: 2021-06-11 / Anne Naumann
+ * last edit / by: 2021-06-12 / Anne Naumann
  */
 import { NgModule } from '@angular/core';
 import { MainRoutingModule } from './main-routing.module';
@@ -18,23 +18,39 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
 
-import { HomeComponent } from './home/home.component';
-import { ShiftsComponent } from './shifts/shifts.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { StatisticsComponent } from './statistics/statistics.component';
-import { ProfileComponent } from './profile/profile.component';
-import { SettingsComponent } from './settings/settings.component';
-import { AdminComponent } from './admin/admin.component';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { SwiperModule, SwiperConfigInterface, SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+
+import { HomeComponent } from './components/home/home.component';
+import { ShiftsComponent } from './components/shifts/shifts.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { ProfileComponent, ProfileDialog } from './components/profile/profile.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { AdminComponent } from './components/admin/admin.component';
 import { MainComponent } from './main.component';
-import { MainNavigationComponent } from './main-navigation/main-navigation.component';
-import { SettingsInfoComponent } from './settings-info/settings-info.component';
-import { SettingsPasswordComponent } from './settings-password/settings-password.component';
-import { SettingsThemeComponent } from './settings-theme/settings-theme.component';
-import { SettingsAdaptScreenComponent } from './settings-adapt-screen/settings-adapt-screen.component';
-import { AdminAddComponent } from './admin-add/admin-add.component';
-import { AdminUserComponent } from './admin-user/admin-user.component';
-import { TasksAddComponent } from './tasks-add/tasks-add.component';
+import { MainNavigationComponent } from './components/main-navigation/main-navigation.component';
+import { SettingsInfoComponent } from './components/settings-info/settings-info.component';
+import { SettingsPasswordComponent } from './components/settings-password/settings-password.component';
+import { SettingsThemeComponent } from './components/settings-theme/settings-theme.component';
+import { SettingsAdaptScreenComponent } from './components/settings-adapt-screen/settings-adapt-screen.component';
+import { AdminAddComponent } from './components/admin-add/admin-add.component';
+import { AdminUserComponent } from './components/admin-user/admin-user.component';
+import { TasksAddComponent } from './components/tasks-add/tasks-add.component';
+import { TasksInputComponent, TasksInputDialog } from './components/tasks-input/tasks-input.component';
+import { TasksTaskComponent } from './components/tasks-task/tasks-task.component';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  observer: true,
+  direction: 'horizontal',
+  threshold: 50,
+  slidesPerView: 1
+};
 
 @NgModule({
   declarations: [
@@ -43,6 +59,7 @@ import { TasksAddComponent } from './tasks-add/tasks-add.component';
     TasksComponent,
     StatisticsComponent,
     ProfileComponent,
+    ProfileDialog,
     SettingsComponent,
     AdminComponent,
     MainComponent,
@@ -53,7 +70,10 @@ import { TasksAddComponent } from './tasks-add/tasks-add.component';
     SettingsAdaptScreenComponent,
     AdminAddComponent,
     AdminUserComponent,
-    TasksAddComponent
+    TasksAddComponent,
+    TasksInputComponent,
+    TasksTaskComponent,
+    TasksInputDialog
   ],
   imports: [
     ViewElementsModule,
@@ -66,7 +86,19 @@ import { TasksAddComponent } from './tasks-add/tasks-add.component';
     MatInputModule,
     MatTabsModule,
     ScrollingModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRippleModule,
+    MatDialogModule,
+    SwiperModule,
+    NgxMaterialTimepickerModule.setLocale('de-DE')
+  ],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
   ]
 })
 export class MainModule { }
